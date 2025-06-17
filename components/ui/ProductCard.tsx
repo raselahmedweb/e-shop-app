e58789c0af81c11fd6e81437881b5252e64484f8
Rasel Ahmed <raselwebdev@hotmail.com>
2025-06-18 02:08:33 +0300

    Dummy data create

---
+++ components/ui/ProductCard.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from 'react-native'
+
+// shadowOffset: {
+//                 width: shadowOffsetWidth,
+//                 height: -shadowOffsetHeight,
+//               },
+//               shadowOpacity,
+//               shadowRadius,
+
+export default function ProductCard() {
+  return (
+    <View>
+      <Text>ProductCard</Text>
+    </View>
+  )
+}
